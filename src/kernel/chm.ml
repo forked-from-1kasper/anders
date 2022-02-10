f54736a7b61c98c2ecfdf6b0574d3bdadc778f0a
Siegmentation Fault <siegmentationfault@yandex.ru>
2022-02-10 23:20:50 +0700

    carefully do “freshExp”

--- src/kernel/chm.ml
+++ src/kernel/chm.ml
@@ -19,25 +19,28 @@ let assign x t e = ctx := Env.add (ident x) (Global, Value t, getTerm e) !ctx
 let promote fn = try fn () with exc -> Error (extErr exc)
 
 let proto : req -> resp = function
-  | Check (e, t)     -> promote (fun () -> check !ctx e (eval !ctx t); OK)
-  | Infer e          -> promote (fun () -> Term (rbV (infer !ctx e)))
-  | Eval e           -> promote (fun () -> Term (rbV (eval !ctx e)))
-  | Conv (e1, e2)    -> promote (fun () ->
+  | Check (e0, t0)     -> promote (fun () -> let t = freshExp t0 in
+    ignore (extSet (infer !ctx t)); check !ctx (freshExp e0) (eval !ctx t); OK)
+  | Infer e            -> promote (fun () -> Term (rbV (infer !ctx e)))
+  | Eval e             -> promote (fun () -> Term (rbV (eval !ctx e)))
+  | Conv (e1, e2)      -> promote (fun () ->
     Bool (conv (eval !ctx e1) (eval !ctx e2)))
-  | Def (x, t, e)    -> promote (fun () ->
+  | Def (x, t0, e0)    -> promote (fun () ->
     if Env.mem (ident x) !ctx then Error (AlreadyDeclared x)
-    else (let t' = eval !ctx (freshExp t) in let e' = freshExp e in
-      ignore (extSet (inferV t')); check !ctx e' t'; assign x t' e'; OK))
-  | Assign (x, t, e) -> promote (fun () ->
+    else (let t = freshExp t0 in let e = freshExp e0 in
+      ignore (extSet (infer !ctx t)); let t' = eval !ctx t in
+      check !ctx e t'; assign x t' e; OK))
+  | Assign (x, t0, e0) -> promote (fun () ->
     if Env.mem (ident x) !ctx then Error (AlreadyDeclared x)
-    else (let t' = eval !ctx (freshExp t) in
-      ignore (extSet (inferV t')); assign x t' (freshExp e); OK))
-  | Assume (x, e)    -> promote (fun () -> let t = eval !ctx (freshExp e) in
+    else (let t = freshExp t0 in ignore (extSet (infer !ctx t));
+          assign x (eval !ctx t) (freshExp e0); OK))
+  | Assume (x, t0)     -> promote (fun () -> let t = freshExp t0 in
     let y = ident x in if Env.mem y !ctx then Error (AlreadyDeclared x)
-    else (ignore (extSet (inferV t)); ctx := Env.add y (Global, Value t, Value (Var (y, t))) !ctx; OK))
-  | Erase x          -> ctx := Env.remove (ident x) !ctx; OK
-  | Wipe             -> ctx := Env.empty; OK
-  | Set (p, x)       ->
+    else (ignore (extSet (infer !ctx t)); let t' = eval !ctx t in
+          ctx := Env.add y (Global, Value t', Value (Var (y, t'))) !ctx; OK))
+  | Erase x            -> ctx := Env.remove (ident x) !ctx; OK
+  | Wipe               -> ctx := Env.empty; OK
+  | Set (p, x)         ->
   begin match p with
     | "trace"       -> Prefs.trace       := getBoolVal p x; OK
     | "pre-eval"    -> Prefs.preeval     := getBoolVal p x; OK
@@ -45,8 +48,8 @@ let proto : req -> resp = function
     | "irrelevance" -> Prefs.irrelevance := getBoolVal p x; OK
     | _             -> Error (InvalidOpt p)
   end
-  | Version          -> Version (1L, 3L, 0L)
-  | Ping             -> Pong
+  | Version            -> Version (1L, 3L, 0L)
+  | Ping               -> Pong
 
 let () = try while true do
   Serialize.resp (try proto (Deserialize.req ())

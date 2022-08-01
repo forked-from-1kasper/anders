6cdf912d46bf0acab625f994ab37cadbf32672e9
Siegmentation Fault <siegmentationfault@yandex.ru>
2022-08-01 20:56:58 +0700

    bump version & remove outdated URLs & CHANGELOG

--- src/kernel/chm.ml
+++ src/kernel/chm.ml
@@ -49,7 +49,7 @@ let proto : req -> resp = function
     | "impredicativity" -> promote (fun () -> Prefs.impredicativity := getUnitVal p x; OK)
     | _                 -> Error (InvalidOpt p)
   end
-  | Version            -> Version (1L, 4L, 2L)
+  | Version            -> Version (1L, 8L, 0L)
   | Ping               -> Pong
 
 let () = try while true do
